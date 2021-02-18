e3ca2a0f421eed27e5f81c060f7872e310887fbe
TiagoDevWeb <tiago.desenvolvedorweb@gmail.com>
2021-02-18 15:42:49 -0300

    chore: infra added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Sendportal\Base\Facades\Sendportal;
+use App\Models\{Workspace, ApiToken, User};
+use Sendportal\Base\Models\{Subscriber, Tag};
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,29 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // $this->call(UserSeeder::class);
+        $user = User::factory()->create([
+            'name' => 'Tiago',
+            'email' => 'tiago@rockbuzz.com.br',
+            'password' => bcrypt(12345678)
+        ]);
+        $workspace = Workspace::factory()->create([
+            'name' => 'Rockbuzz',
+            'owner_id' => $user->id
+        ]);
+        $user->update(['current_workspace_id' => $workspace->id]);
+        auth()->login($user);
+        ApiToken::factory()->create([
+            'workspace_id' => Sendportal::currentWorkspaceId()
+        ]);
+    
+        Tag::factory()->count(10)->create([
+            'workspace_id' => Sendportal::currentWorkspaceId()
+        ])->each(function ($tag) {
+            Subscriber::factory()->count(rand(30, 60))->create([
+                'workspace_id' => Sendportal::currentWorkspaceId()
+            ])->each(function ($subscriber) use ($tag) {
+                $subscriber->tags()->attach($tag);
+            });
+        });
     }
 }
