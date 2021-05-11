4d6b3a49a9ce04063b3954996385e5fc4dace0dd
Paulo Scheuermann <paulo@rockbuzz.com.br>
2021-05-11 10:24:34 -0400

    fiz: correções e melhorias no controller de reports na api.

--- app/Http/Controllers/Api/CampaignReportsController.php
+++ app/Http/Controllers/Api/CampaignReportsController.php
@@ -5,13 +5,11 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Api;
 
 use Exception;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
-use Illuminate\View\View;
-use Sendportal\Base\Facades\Sendportal;
-use Sendportal\Base\Http\Controllers\Controller;
 use Sendportal\Base\Models\Campaign;
+use Sendportal\Base\Facades\Sendportal;
+use Illuminate\Http\{Request, JsonResponse};
+use Sendportal\Base\Http\Controllers\Controller;
 use Sendportal\Base\Presenters\CampaignReportPresenter;
 use Sendportal\Base\Repositories\Campaigns\CampaignTenantRepositoryInterface;
 use Sendportal\Base\Repositories\Messages\MessageTenantRepositoryInterface;
@@ -33,42 +31,48 @@ class CampaignReportsController extends Controller
     }
 
     /**
-     * Show campaign report view.
-     *
-     * @return RedirectResponse|View
+     * @param int $id
+     * @param Request $request
+     * @return JsonResponse
      * @throws Exception
      */
-    public function index(int $id, Request $request)
+    public function index(int $id, Request $request): JsonResponse
     {
         $campaign = $this->campaignRepo->find(Sendportal::currentWorkspaceId(), $id);
 
-        $this->statusCheck($campaign);
+        if ($response = $this->statusCheck($campaign)) {
+            return $response;
+        }
 
-        $presenter = new CampaignReportPresenter($campaign, Sendportal::currentWorkspaceId(), (int) $request->get('interval', 24));
-        $presenterData = $presenter->generate();
+        $presenterData = (new CampaignReportPresenter(
+            $campaign,
+            Sendportal::currentWorkspaceId(),
+            (int)$request->get('interval', 24)
+        ))->generate();
 
         $data = [
             'campaign' => $campaign,
-            'campaignUrls' => $presenterData['campaignUrls'],
-            'campaignStats' => $presenterData['campaignStats'],
-            'chartLabels' => json_encode(Arr::get($presenterData['chartData'], 'labels', [])),
-            'chartData' => json_encode(Arr::get($presenterData['chartData'], 'data', [])),
+            'campaign_urls' => $presenterData['campaignUrls'],
+            'campaign_stats' => $presenterData['campaignStats'],
+            'chart_labels' => json_encode(Arr::get($presenterData['chartData'], 'labels', [])),
+            'chart_data' => json_encode(Arr::get($presenterData['chartData'], 'data', [])),
         ];
 
         return response()->json(['data' => $data]);
     }
 
     /**
-     * Show campaign recipients.
-     *
-     * @return RedirectResponse|View
+     * @param int $id
+     * @return JsonResponse
      * @throws Exception
      */
-    public function recipients(int $id)
+    public function recipients(int $id): JsonResponse
     {
         $campaign = $this->campaignRepo->find(Sendportal::currentWorkspaceId(), $id);
 
-        $this->statusCheck($campaign);
+        if ($response = $this->statusCheck($campaign)) {
+            return $response;
+        }
 
         $messages = $this->messageRepo->recipients(Sendportal::currentWorkspaceId(), Campaign::class, $id);
 
@@ -81,64 +85,72 @@ class CampaignReportsController extends Controller
     }
 
     /**
-     * Show campaign opens.
-     *
-     * @return RedirectResponse|View
+     * @param int $id
+     * @return JsonResponse
      * @throws Exception
      */
-    public function opens(int $id)
+    public function opens(int $id): JsonResponse
     {
+        /** @var Campaign $campaign */
         $campaign = $this->campaignRepo->find(Sendportal::currentWorkspaceId(), $id);
         $averageTimeToOpen = $this->campaignRepo->getAverageTimeToOpen($campaign);
 
-        $this->statusCheck($campaign);
+        if ($response = $this->statusCheck($campaign)) {
+            return $response;
+        }
 
         $messages = $this->messageRepo->opens(Sendportal::currentWorkspaceId(), Campaign::class, $id);
+
+        $campaign->append(['unique_open_count', 'total_open_count']);
 
         return response()->json([
             'data' => [
                 'campaign' => $campaign,
                 'messages' => $messages,
-                'averageTimeToOpen' => $averageTimeToOpen
+                'average_time_to_open' => $averageTimeToOpen
             ]
         ]);
     }
 
     /**
-     * Show campaign clicks.
-     *
-     * @return RedirectResponse|View
+     * @param int $id
+     * @return JsonResponse
      * @throws Exception
      */
-    public function clicks(int $id)
+    public function clicks(int $id): JsonResponse
     {
         $campaign = $this->campaignRepo->find(Sendportal::currentWorkspaceId(), $id);
         $averageTimeToClick = $this->campaignRepo->getAverageTimeToClick($campaign);
 
-        $this->statusCheck($campaign);
+        if ($response = $this->statusCheck($campaign)) {
+            return $response;
+        }
 
         $messages = $this->messageRepo->clicks(Sendportal::currentWorkspaceId(), Campaign::class, $id);
+
+        $campaign->append(['unique_click_count', 'total_click_count']);
 
         return response()->json([
             'data' => [
                 'campaign' => $campaign,
                 'messages' => $messages,
-                'averageTimeToClick' => $averageTimeToClick
+                'average_time_to_click' => $averageTimeToClick
             ]
         ]);
     }
 
     /**
-     * Show campaign bounces.
-     *
-     * @return RedirectResponse|View
+     * @param int $id
+     * @return JsonResponse
      * @throws Exception
      */
-    public function bounces(int $id)
+    public function bounces(int $id): JsonResponse
     {
         $campaign = $this->campaignRepo->find(Sendportal::currentWorkspaceId(), $id);
 
-        $this->statusCheck($campaign);
+        if ($response = $this->statusCheck($campaign)) {
+            return $response;
+        }
 
         $messages = $this->messageRepo->bounces(Sendportal::currentWorkspaceId(), Campaign::class, $id);
 
@@ -151,16 +163,17 @@ class CampaignReportsController extends Controller
     }
 
     /**
-     * Show campaign unsubscribes.
-     *
-     * @return RedirectResponse|View
+     * @param int $id
+     * @return JsonResponse
      * @throws Exception
      */
-    public function unsubscribes(int $id)
+    public function unsubscribes(int $id): JsonResponse
     {
         $campaign = $this->campaignRepo->find(Sendportal::currentWorkspaceId(), $id);
 
-        $this->statusCheck($campaign);
+        if ($response = $this->statusCheck($campaign)) {
+            return $response;
+        }
 
         $messages = $this->messageRepo->unsubscribes(Sendportal::currentWorkspaceId(), Campaign::class, $id);
 
@@ -172,6 +185,10 @@ class CampaignReportsController extends Controller
         ]);
     }
 
+    /**
+     * @param Campaign $campaign
+     * @return JsonResponse
+     */
     private function statusCheck(Campaign $campaign)
     {
         if ($campaign->draft) {
